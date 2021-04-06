1b1938e627bfad1ba876bce3c9682c640ee33eef
Cyril Walle <cyril.walle@protonmail.com>
2021-04-06 20:19:44 +0200

    better handle nesting of code inside links

--- source/markdown/MarkdownParser.js
+++ source/markdown/MarkdownParser.js
@@ -15,11 +15,11 @@ const STATE = {
     TITLE_TEXT: i++,
     LIST_ITEM_TEXT: i++,
     LIST_ITEM_END: i++,
+    LINK_TEXT: i++,
 };
 
 const INLINE_STATE = {
     REGULAR: i++,
-    LINK_TEXT: i++,
     AFTER_LINK_TEXT: i++,
     LINK_TARGET: i++,
 }
@@ -70,8 +70,12 @@ class MarkdownParser extends Transform {
                 this._refresh();
                 this.state = STATE.FREE;
                 break;
-            case STATE.RAW:
-                const codeBlock = `<code class="${this.languagePrefix}${this.rawDescription}">${this.currentString}</code>`
+            case STATE.RAW: {
+                let classText = ``;
+                if (this.rawDescription) {
+                    classText = `class="${this.languagePrefix}${this.rawDescription}"`;
+                }
+                const codeBlock = `<code${classText}>${this.currentString}</code>`
                 let currentString;
                 if (this.closingBackTicks === 3) {
                     currentString = `<pre>${codeBlock}</pre>`;
@@ -87,7 +91,7 @@ class MarkdownParser extends Transform {
                     this.state = STATE.FREE;
                 }
                 break;
-            case STATE.LIST_ITEM_TEXT:
+            } case STATE.LIST_ITEM_TEXT:
                 this.items.push(this.currentString);
             case STATE.LIST_ITEM_END:
                 toPush.push(`<ul>`);
@@ -103,39 +107,50 @@ class MarkdownParser extends Transform {
 
     }
 
-    _handleInline(c) {
+    _handleInline(c, toPush) {
         switch (this.inlineState) {
-            case INLINE_STATE.LINK_TEXT:
-                if (c === `]`) {
-                    this.inlineState = INLINE_STATE.AFTER_LINK_TEXT;
-                } else {
-                    this._selfInlineBuffer(c);
-                }
-                break;
             case INLINE_STATE.AFTER_LINK_TEXT:
                 if (c === `(`) {
                     this.inlineState = INLINE_STATE.LINK_TARGET;
-                    this.linkText = this.currentInlineString;
+                    this.linkText = this.currentString;
                 } else {
                     // not a link just regular text inside []
                     this._selfInlineBuffer(c);
-                    this._selfBuffer(`[${this.currentInlineString}`);
+                    this._selfBuffer(`[${this.currentString}`);
                     this.inlineState = INLINE_STATE.REGULAR;
+                    // we already poped before
                 }
-                this.currentInlineString = ``;
+                this.currentString = ``;
                 break;
             case INLINE_STATE.LINK_TARGET:
                 if (c === `)`) {
-                    this._selfBuffer(`<a href=${this.currentInlineString}>${this.linkText}</a>`);
+                    // this._closeCurrent(toPush); // cannot close current since it is an inline state
+                    this.currentString = `<a href=${this.currentInlineString}>${this.linkText}</a>`;
                     this.inlineState = INLINE_STATE.REGULAR;
-                    this.currentInlineString = ``;
+                    this.currentInlineString = ``;                    
                 } else {
                     this._selfInlineBuffer(c);
                 }
                 break;
             default:
-                return true;
+                if (c === `\``) {
+                    this.inside.push(this.state);
+                    if (this.state === STATE.FREE) {
+                        this.inside.push(STATE.TEXT);
+                    }
+                    this.state = STATE.START_RAW;
+                    this.backTicks = 1;
+                } else if (c === `[`) {
+                    this.inside.push(this.state);
+                    if (this.state === STATE.FREE) {
+                        this.inside.push(STATE.TEXT);
+                    }
+                    this.state = STATE.LINK_TEXT;
+                } else {
+                    return true;
+                }
         }
+
     }
 
     _transform(buffer, _, done) {
@@ -145,38 +160,30 @@ class MarkdownParser extends Transform {
 
         for (let i = 0; i < length; i += 1) {
             const c = asString[i];
+            console.log(this.state, c)
             switch (this.state) {
                 case STATE.FREE:
+                    if (!this._handleInline(c, toPush)) {
+                        continue;
+                    }
                     if (c === `#`) {
                         this.state = STATE.START_TITLE;
                         this.titleLevel = 1;
-                    } else if (c === `\``) {
-                        this.state = STATE.START_RAW;
-                        this.backTicks = 1;
                     } else if (c === `*` || c === `-`) {
                         this.state = STATE.LIST_ITEM_TEXT;
                     } else if (isWhitespace(c)) {
 
-                    } else if (c === `[`) {
-                        this.inlineState = INLINE_STATE.LINK_TEXT;
-                        this.state = STATE.TEXT
                     } else {
                         this._selfBuffer(c);
-                        this.state = STATE.TEXT
+                        this.state = STATE.TEXT;
                     }
                     break;
 
                 case STATE.TEXT:
-                    if (!this._handleInline(c)) {
+                    if (!this._handleInline(c, toPush)) {
                         continue;
                     }
-                    if (c === `\``) {
-                        this.inside.push(STATE.TEXT);
-                        this.state = STATE.START_RAW;
-                        this.backTicks = 1;
-                    } else if (c === `[`) {
-                        this.inlineState = INLINE_STATE.LINK_TEXT;
-                    } else if (c === `\n`) {
+                     if (c === `\n`) {
                         if (this.newLined) {
                             this._closeCurrent(toPush);
                         } else {
@@ -190,21 +197,39 @@ class MarkdownParser extends Transform {
                         this._selfBuffer(c);
                     }
                     break;
+                case STATE.LINK_TEXT:
+                    if (!this._handleInline(c, toPush)) {
+                        continue;
+                    }
+                    if (c === `]`) {
+                        this.state = this.inside.pop() || STATE.FREE;
+                        this.inlineState = INLINE_STATE.AFTER_LINK_TEXT;
+                    } else {
+                        this._selfBuffer(c);
+                    }
+                    break;
                 case STATE.LIST_ITEM_TEXT:
-                    if (!this._handleInline(c)) {
+                    if (!this._handleInline(c, toPush)) {
                         continue;
                     }
                     if (c === `\n`) {
                         this.items.push(this.currentString)
                         this._refresh();
                         this.state = STATE.LIST_ITEM_END;
-                    } else if (c === `[`) {
-                        this.inlineState = INLINE_STATE.LINK_TEXT;
                     } else {
                         this._selfBuffer(c);
                     }
                     break;
-
+                case STATE.TITLE_TEXT:
+                    if (!this._handleInline(c, toPush)) {
+                        continue;
+                    }
+                    if (c === `\n`) {
+                        this._closeCurrent(toPush);
+                    } else {
+                        this._selfBuffer(c);
+                    }
+                    break;
                 case STATE.LIST_ITEM_END:
                     if (c === `\n`) {
                         this._closeCurrent(toPush);
@@ -216,22 +241,15 @@ class MarkdownParser extends Transform {
                 case STATE.START_TITLE:
                     if (c === `#`) {
                         this.titleLevel += 1;
-                    } else {
+                    } else if (isWhitespace(c)) {
                         this.state = STATE.TITLE_TEXT;
-                    }
-                    break;
-                case STATE.TITLE_TEXT:
-                    if (!this._handleInline(c)) {
-                        continue;
-                    }
-                    if (c === `\n`) {
-                        this._closeCurrent(toPush);
-                    } else if (c === `[`) {
-                        this.inlineState = INLINE_STATE.LINK_TEXT;
                     } else {
-                        this._selfBuffer(c);
+                        //malformed title
+                        this._selfBuffer(`${"#".repeat(this.titleLevel)}${c}`);
+                        this.state = STATE.TEXT;
                     }
                     break;
+
                 case STATE.START_RAW:
                     if (c === `\``) {
                         this.backTicks += 1;
@@ -260,6 +278,10 @@ class MarkdownParser extends Transform {
                             this._closeCurrent(toPush);
                         }
                     } else {
+                        if (this.closingBackTicks) {
+                            this._selfBuffer(`\``.repeat(this.closingBackTicks));
+                            this.closingBackTicks = 0;
+                        }
                         this._selfBuffer(c);
                     }
                     break;
