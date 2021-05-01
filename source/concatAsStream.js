dc483cd500664d7db9b831219cb57a199397b4db
Cyril Walle <cyril.walle@protonmail.com>
2021-05-01 21:41:38 +0200

    fix concatAsStream not working with MarkdownParser

--- source/concatAsStream.js
+++ source/concatAsStream.js
@@ -1,7 +1,8 @@
 export { concatAsStream };
 
 import { Readable } from "stream";
-
+import MultiStream from "multistream";
+import intoStream from 'into-stream';
 
 const isStream = (x) => {
     return x && typeof x !== `string` && typeof x.destroy === `function`; 
@@ -12,64 +13,14 @@ const isPromise = (x) => {
 };
 
 const concatAsStream = (things, options = {}) => {
-    let currentThing;
-    let wantsToRead = true;
-    const next = () => {
-        currentThing = things.shift();
-    };
-    const readTheStream = function (size) {
-        let result;
-        while (wantsToRead && (result = currentThing.read(size))) {
-            wantsToRead = this.push(result);
+    return new MultiStream(things.map(x => {
+        if (isPromise(x)) {
+            return intoStream(x);
         }
-    };
-    const attachedMap = new WeakSet();
-    next();
-    return new Readable({
-        read(size) {
-            if (!currentThing) {
-                this.push(null);
-                return;
-            }
-            if (isPromise(currentThing)) {
-                if (!attachedMap.has(currentThing)) {
-                    currentThing.then(value => {
-                        currentThing = value;
-                        this.push(``);// force
-                    });
-                    attachedMap.add(currentThing);
-                }
-                return;
-            }
-            if (isStream(currentThing)) {
-                if (!attachedMap.has(currentThing)) {
-                    currentThing.on(`readable`, () => {
-                        readTheStream.call(this, size);
-                    });
-                    currentThing.once(`end`, () => {
-                        wantsToRead = true;
-                        next();
-                    });
-                    attachedMap.add(currentThing);
-                } else if (!wantsToRead) {
-                    wantsToRead = true;
-                    readTheStream.call(this, size);
-                }
-                return;
-            }
-            this.push(currentThing);
-            next();
-            
-        },
-        destroy(error, callback) {
-            for (let k = 0; k < things.length; k += 1) {
-                if (isStream(things[k])) {
-                    things[k].destroy();
-                }
-            }
-            callback(error);
-        },
-        // highWaterMark: 1,
-        ...options,
-    });
+        if (isStream(x)) {
+            return x;
+        }
+        return Readable.from(x);
+        
+    }));
 };
