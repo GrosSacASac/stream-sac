2519a6f3f538e2a0900252c1b28f7acb7e0c2ee6
Walle Cyril <cyril.walle@protonmail.com>
2021-05-29 18:35:35 +0200

    Update MarkdownParserTemp.js

--- tests/specification/MarkdownParserTemp.js
+++ tests/specification/MarkdownParserTemp.js
@@ -367,6 +367,50 @@ test(`raw html code is displayed properly`, async t => {
 &lt;/style&gt;</code></pre>`));
 });
 
+test(`raw and titles`, async t => {
+    const markdownParser = new MarkdownParser();
+    concatAsStream([`
+## a
+
+\`\`\`
+b
+\`\`\`
+
+## d
+
+\`\`\`e
+f
+\`\`\`
+`]).pipe(markdownParser);
+
+    let forceBuffer = ``
+    markdownParser.on('data', (x) => {
+        forceBuffer = `${forceBuffer}${x}`;
+    });
+    await finished(markdownParser);  
+    t.is(forceBuffer, (`<h2>a</h2><pre><code>b</code></pre><h2>d</h2><pre><code class="language-e">f</code></pre>`));
+});
+
+test(`raw and titles 2`, async t => {
+    const markdownParser = new MarkdownParser();
+    concatAsStream([`### a
+
+\`\`\`b
+c
+\`\`\`
+
+### \`d\`
+
+e`]).pipe(markdownParser);
+
+    let forceBuffer = ``
+    markdownParser.on('data', (x) => {
+        forceBuffer = `${forceBuffer}${x}`;
+    });
+    await finished(markdownParser);  
+    t.is(forceBuffer, (`<h3>a</h3><pre><code class="language-b">c</code></pre><h3><code>d</code></h3><p>e</p>`));
+});
+
 test(`image`, async t => {
     const markdownParser = new MarkdownParser();
     const altText = `drinking face`
