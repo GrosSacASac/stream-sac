3ec5a1b8fa7825d3b66a8cd108b3a4d473f7aa17
Walle Cyril <cyril.walle@protonmail.com>
2021-10-29 15:46:44 +0200

    test

--- tests/specification/MarkdownParserTemp.js
+++ tests/specification/MarkdownParserTemp.js
@@ -5,17 +5,37 @@ import { finished } from "stream/promises";
 import slugify from "@sindresorhus/slugify";
 //todo change order (block then inline)
 
+// test(`it should handle empty html elements`, async t => {
+//     const markdownParser = new MarkdownParser();
+//     concatAsStream([`<img src="a" alt="b">
 
+//     _c_`]).pipe(markdownParser);
 
-test(`inline html stays as is `, async t => {
+//     let forceBuffer = ``
+//     markdownParser.on('data', (x) => {
+//         forceBuffer = `${forceBuffer}${x}`;
+//     });
+//     await finished(markdownParser);
+//     t.is(forceBuffer, (`<img src="a" alt="b"><p><em>c</em></p>`));
+// });
+
+test(`it should not mix elements`, async t => {
     const markdownParser = new MarkdownParser();
-    concatAsStream([`
-    <p>8 &gt; 7</p>`]).pipe(markdownParser);
+    concatAsStream([`# a
+
+    <img src="b" alt="c">
+    
+    _d_
+    
+    ## e
+    `]).pipe(markdownParser);
 
     let forceBuffer = ``
     markdownParser.on('data', (x) => {
         forceBuffer = `${forceBuffer}${x}`;
     });
     await finished(markdownParser);
-    t.is(forceBuffer, (`<p>8 &gt; 7</p>`));
+    t.is(forceBuffer, (`<h1>a</h1><img src="b" alt="c"><p><em>d</em></p><h2>e</h2>`));
 });
+
+
