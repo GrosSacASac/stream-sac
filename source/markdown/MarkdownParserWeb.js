f5253c60b020d3cb2474b34309853dda955c8c41
Cyril Walle <cyril.walle@protonmail.com>
2021-11-10 16:39:00 +0100

    make push = enqueue

--- source/markdown/MarkdownParserWeb.js
+++ source/markdown/MarkdownParserWeb.js
@@ -5,6 +5,7 @@ import { flush, transform, start } from "./MarkdownParser.js";
 const createMarkdownParserStream = (options = {}) => {
     return new TransformStream({
         start(controller) {
+            controller.push = controller.enqueue.bind(controller);
             start(controller, options);
         },
 
