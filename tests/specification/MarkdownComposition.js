38b28549aa996bafd29ba525825adfd532b304d2
Cyril Walle <cyril.walle@protonmail.com>
2022-01-16 23:22:28 +0100

    Update MarkdownComposition.js

--- tests/specification/MarkdownComposition.js
+++ tests/specification/MarkdownComposition.js
@@ -18,7 +18,6 @@ test(`em in the middle of list items`, async t => {
         forceBuffer = `${forceBuffer}${x}`;
     });
     await finished(markdownParser);
-    // t.is(forceBuffer, (`<a href="${linkTarget}">${linkText}</a>`));
     const li1 = `<li><em>${a}</em></li>`;
     const li2 = `<li>${outside}<em>${b}</em>${outside}</li>`;
     t.is(forceBuffer, (`<ul>${li1}${li2}</ul>`));
@@ -41,7 +40,6 @@ test(`em in the middle of list items 2`, async t => {
         forceBuffer = `${forceBuffer}${x}`;
     });
     await finished(markdownParser);
-    // t.is(forceBuffer, (`<a href="${linkTarget}">${linkText}</a>`));
     const li1 = `<li><em>${a}</em></li>`;
     const li2 = `<li>${outside}<em>${b}</em>${outside}</li>`;
     t.is(forceBuffer, (`<ul>${li1}${li2}${li2}${li2}${li2}</ul>`));
